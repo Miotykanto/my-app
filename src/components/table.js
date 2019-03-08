e0a3fd0e88108138dc4c5861cdbb3455e79c3bdc
miotykanto <mirantotefy@gmail.com>
2019-03-08 16:20:28 +0300

    envoie

---
+++ src/components/table.js
@@ -0,0 +1,17 @@
+import React, {Component } from "react";
+
+class table extends Component {
+  render() {
+    return (
+      <div class="table"> 
+        <ul>
+          <li>id</li>
+          <li>Nom</li>
+          <li>prenom</li>
+        </ul>
+      </div>
+      
+    );
+  }
+}
+export default table;
